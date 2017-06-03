14e82417acc4eb435d1d713142fb699c317e539b
Kyle Barrow <kyle@barrow.io>
2017-06-03 15:58:02 +0900

    Update gulp, tests

--- chibi.js
+++ chibi.js
@@ -1,4 +1,4 @@
-/*!chibi 3.0.8, Copyright 2012-2016 Kyle Barrow, released under MIT license */
+/*!chibi 3.0.9, Copyright 2012-2017 Kyle Barrow, released under MIT license */
 (function () {
 	'use strict';
 
